51dcac7e313c0e119ab4b2080ae54c34ee192852
abel masingita <60102940+abelmasingita@users.noreply.github.com>
2022-07-06 01:04:51 +0200

    estore mobile app initialization

--- src/App.tsx
+++ src/App.tsx
@@ -10,67 +10,75 @@ import {
   setupIonicReact
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
-import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
+import { home, search, cart, personOutline } from 'ionicons/icons'
+import Home from './pages/Home'
+import Categories from './pages/Categories'
+import Account from './pages/Account'
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import '@ionic/react/css/core.css'
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import '@ionic/react/css/normalize.css'
+import '@ionic/react/css/structure.css'
+import '@ionic/react/css/typography.css'
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+import '@ionic/react/css/padding.css'
+import '@ionic/react/css/float-elements.css'
+import '@ionic/react/css/text-alignment.css'
+import '@ionic/react/css/text-transformation.css'
+import '@ionic/react/css/flex-utils.css'
+import '@ionic/react/css/display.css'
 
 /* Theme variables */
-import './theme/variables.css';
+import './theme/variables.css'
+import Cart from './pages/Cart'
 
-setupIonicReact();
+setupIonicReact()
 
 const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonTabs>
         <IonRouterOutlet>
-          <Route exact path="/tab1">
-            <Tab1 />
+          <Route exact path='/home'>
+            <Home />
           </Route>
-          <Route exact path="/tab2">
-            <Tab2 />
+          <Route exact path='/categories'>
+            <Categories />
           </Route>
-          <Route path="/tab3">
-            <Tab3 />
+          <Route exact path='/cart'>
+            <Cart />
           </Route>
-          <Route exact path="/">
-            <Redirect to="/tab1" />
+          <Route exact path='/account'>
+            <Account />
+          </Route>
+          <Route exact path='/'>
+            <Redirect to='/home' />
           </Route>
         </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
+        <IonTabBar slot='bottom'>
+          <IonTabButton tab='Home' href='/home'>
+            <IonIcon icon={home} />
+            <IonLabel>Home</IonLabel>
           </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
+          <IonTabButton tab='Categories' href='/categories'>
+            <IonIcon icon={search} />
+            <IonLabel>Categories</IonLabel>
           </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon icon={square} />
-            <IonLabel>Tab 3</IonLabel>
+          <IonTabButton tab='Cart' href='/cart'>
+            <IonIcon icon={cart} />
+            <IonLabel>Cart</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab='Account' href='/account'>
+            <IonIcon icon={personOutline} />
+            <IonLabel>Account</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
     </IonReactRouter>
   </IonApp>
-);
+)
 
 export default App;
